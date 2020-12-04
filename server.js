273fd97cc12e796e743135591b54a104834e9a50
unknown <zachelms26@gmail.com>
2020-12-04 15:43:03 -0800

    Bare bones server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-/**
- * Start of the express package code.
- */
+/*****************
+ *
+ * Restaurant Tinder
+ *
+ ***************/
 
- 
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = 3000;
