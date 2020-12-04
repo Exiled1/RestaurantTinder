af765ff883d67679b1024c402b1dfdce9e3dc775
unknown <zachelms26@gmail.com>
2020-12-04 15:45:57 -0800

    Added more bones

--- server.js
+++ server.js
@@ -10,3 +10,19 @@ var exphbs = require('express-handlebars');
 
 var app = express();
 var port = 3000;
+
+app.get('/', function (req, res) {
+  console.log("CS290 work in project");
+  res.status(200);
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  console.log("ERROR");
+  res.status(404);
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
