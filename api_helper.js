151266e4e8b3f638023900e96b1b9d8784fbaf83
exiled1 <wazzupbuddy34@gmail.com>
2020-12-05 15:57:04 -0800

    Deleted index.html and moved it into the HB homepage.
    Also added a comment to helper code

--- api_helper.js
+++ api_helper.js
@@ -1,10 +1,13 @@
+/**
+ * This is Rudy's API Helper module, it takes a user's current location and returns all of the nearby data that we need to make the app work.
+ */
 const {Client} = require("@googlemaps/google-maps-services-js");
 
 const client = new Client({}); // Google API Client should be private per instance that I use it.
 client.placesNearby(
     {
         params: {
-            
+
         }
     }
 );
