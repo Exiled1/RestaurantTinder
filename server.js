18ac8b6d36bd255d54571111c94ec5fd26203711
exiled1 <wazzupbuddy34@gmail.com>
2020-12-05 15:50:00 -0800

    Added helper functions, updated package, fixed conflicts on server.js
    Added:
    + api_helper.js - This is a helper package for zach to work on the node.js server side.
    + installed google server-side api package

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /*****************
  *
- * Restauranteur
+ * Restaurant Tinder Server Code.
  *
  ***************/
 
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
+
+
 
 var app = express();
 var port = 3000;
@@ -21,11 +23,16 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-app.get('*', function (req, res) {
-  console.log("ERROR");
-  res.status(404);
+app.get('/', function(req, res) {
+    console.log("CS290 work in project");
+    res.status(200);
 });
 
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+app.get('*', function(req, res) {
+    console.log("ERROR");
+    res.status(404);
+});
+
+app.listen(port, function() {
+    console.log("== Server is listening on port", port);
 });
