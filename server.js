89a3ddd761c8be3522bf29a31a8d5487d5339d4e
zelms <zjelms@gmail.com>
2020-12-08 07:37:02 -0800

    Troubleshooting

--- server.js
+++ server.js
@@ -15,17 +15,17 @@ var port = 3000;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
 
+app.get('/', async function (req, res) {
+  // let apiHelper = new APIHelper(latitudeLongitude, 6000);
+  //
+  // let placesObj = await apiHelper.apiInit();
+  console.log("Request received");
+  res.status(200).send();
 
-app.get('/', function (req, res) {
-  let apiHelper = new APIHelper(latitudeLongitude, 6000);
-
-  let placesObj = await apiHelper.apiInit();
-
-  res.status(200).render('homepage', {
-    placesObj: placesObj
-  });
+  // res.status(200).render('homepage', {
+  //   placesObj: placesObj
+  // });
 });
 
 app.get('/test', async function (req, res) {
@@ -40,18 +40,20 @@ app.get('/test', async function (req, res) {
     res.status(200);
 });
 
-app.post('/rightRestaurant', function (req, res)
+app.post('/rightRestaurant', async function (req, res)
 {
   let apiHelper = new APIHelper(latitudeLongitude, 6000);
 
   let placesObj = await apiHelper.apiInit();
+
+  // res.status(200).send("Works");
 
   res.status(200).render('homepage', {    //This should at some point actually make it so that restrauntprofile.handlebars is displayed
     placesObj: placesObj                  //Modification probably needs to be in the homepage.handlebars file
   });
 });
 
-app.post('/leftRestaurant', function (req, res)
+app.post('/leftRestaurant', async function (req, res)
 {
   let apiHelper = new APIHelper(latitudeLongitude, 6000);
 
@@ -62,10 +64,11 @@ app.post('/leftRestaurant', function (req, res)
   });
 });
 
-app.get('*', function(req, res) {
-    console.log("== 404 Page Would Be displayed");
-    res.status(404);
-});
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).send('404');
+});;
 
 app.listen(port, function() {
     console.log("== Server is listening on port", port);
