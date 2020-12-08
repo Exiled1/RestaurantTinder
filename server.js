a3773181eff5237071ae1380802880cceb80e392
exiled1 <wazzupbuddy34@gmail.com>
2020-12-08 00:33:43 -0800

    Alright, this is a big one. Anyways, I **massively** overhauled my API Helper module and refactored the server.js again, more details in the description.
    Okay, I know that most of my commits have been me changing the API Helper class, I swear it's getting close to perfect right now so I'm going to slow down on any drastic changes and just focus on adding functionality to it.
    
    Anyways, without further ado:
    Changes:
    + Completely changed how you instantiate the API Helper class, now the only thing you need to call is the createAPIHelper function and you have full access to all the functionality of the API Helper class, without all the annoying setup, so it's much more streamlined.
    
    + I placed the entire class inside of my createAPIHelper function, why you ask? Well because I was getting tired of being able to directly being able to instantiate an API instance, so I made it a little harder to access directly. However, because JS is JS, it's still *technically* public, it'd take a lot of digging though.
    + Added a getRandomRestaurant function, it's non functioning right now and only console.log's the apt data object, but the intended use for this class will be to get back a singular restaurant object, and because it's a single restaurant object you'll be able to send it back to my create API Helper function and get specific details with the Google Place Details API.
    
    Planned Changes:
    + Make a way to start up the Place Details API and return specific restaurant information.

--- server.js
+++ server.js
@@ -7,69 +7,63 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-const APIHelper = require('./APIHelper');
+const createAPIHelper = require('./APIHelper');
 const latitudeLongitude = [45.409274, -122.722615];
 var app = express();
 var port = 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
 
 
 app.get('/', async function (req, res) {
-  // let apiHelper = new APIHelper(latitudeLongitude, 6000);
-  //
-  // let placesObj = await apiHelper.apiInit();
-  console.log("Request received");
-  res.status(200).send();
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
+    apiHelper.getRandomRestaurant();
 
-  // res.status(200).render('homepage', {
-  //   placesObj: placesObj
-  // });
+    res.status(200).render('homepage', {
+        placesObj: placesObj
+    });
 });
 
 app.get('/test', async function (req, res) {
 
-    let apiHelper = new APIHelper(latitudeLongitude, 3000);
-    // currently focused on a random albersons
-    // with a 3000 mile radius.
-
-    let placesObj = await apiHelper.apiInit(); // this initializes the API Helper methods.
-
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
     apiHelper.getRandomRestaurant();
+
     res.status(200);
 });
 
-app.post('/rightRestaurant', async function (req, res)
-{
-  let apiHelper = new APIHelper(latitudeLongitude, 6000);
+app.post('/rightRestaurant', async function (req, res) {
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000); // creates an instance of the API Helper class.
 
-  let placesObj = await apiHelper.apiInit();
+    let placesObj = apiHelper.apiDataObject; // this gets the places object.
 
-  // res.status(200).send("Works");
-
-  res.status(200).render('homepage', {    //This should at some point actually make it so that restrauntprofile.handlebars is displayed
-    placesObj: placesObj                  //Modification probably needs to be in the homepage.handlebars file
-  });
+    res.status(200).render('homepage', { //This should at some point actually make it so that restrauntprofile.handlebars is displayed
+        placesObj: placesObj //Modification probably needs to be in the homepage.handlebars file
+    });
 });
 
-app.post('/leftRestaurant', async function (req, res)
-{
-  let apiHelper = new APIHelper(latitudeLongitude, 6000);
+app.post('/leftRestaurant', async function (req, res) {
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
 
-  let placesObj = await apiHelper.apiInit();
+    let placesObj = apiHelper.apiDataObject; 
 
-  res.status(200).render('homepage', {
-    placesObj: placesObj
-  });
+    res.status(200).render('homepage', {
+        placesObj: placesObj
+    });
 });
-
-app.use(express.static('public'));
 
 app.get('*', function (req, res) {
-  res.status(404).send('404');
-});;
+    console.log("== 404 Page Would Be displayed");
+    res.status(404);
+});
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("== Server is listening on port", port);
 });
