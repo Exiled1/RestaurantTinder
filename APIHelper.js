8bc074fe19641f7e7e234cfd7b2917c6652d95f5
exiled1 <wazzupbuddy34@gmail.com>
2020-12-08 00:33:43 -0800

    Alright, this is a big one. Anyways, I **massively** overhauled my API Helper module and refactored the server.js again, more details in the description.
    Okay, I know that most of my commits have been me changing the API Helper class, I swear it's getting close to perfect right now so I'm going to slow down on any drastic changes and just focus on adding functionality to it.
    
    Anyways, without further ado:
    Changes:
    + Completely changed how you instantiate the API Helper class, now the only thing you need to call is the createAPIHelper function and you have full access to all the functionality of the API Helper class, without all the annoying setup, so it's much more streamlined.
    
    + I placed the entire class inside of my createAPIHelper function, why you ask? Well because I was getting tired of being able to directly being able to instantiate an API instance, so I made it a little harder to access directly. However, because JS is JS, it's still *technically* public, it'd take a lot of digging though.
    + Added a getRandomRestaurant function, it's non functioning right now and only console.log's the apt data object, but the intended use for this class will be to get back a singular restaurant object, and because it's a single restaurant object you'll be able to send it back to my create API Helper function and get specific details with the Google Place Details API.
    
    Planned Changes:
    + Make a way to start up the Place Details API and return specific restaurant information.

--- APIHelper.js
+++ APIHelper.js
@@ -1,68 +1,101 @@
 /**
  * This is Rudy's API Helper module, it takes a user's current 
  * location and returns all of the nearby data that we need to make the app work.
+ * To use my API Helper you just need to await the relevant createAPIHelper function and it'll 
+ * create an instance of the APIHelper class, from there you can use all the class methods
+ * without any trouble. You can also access the apiDataObject from this as well.
+ * One thing to watch out for though, is that if you're using this in a function like a middleware
+ * function, remember to use the **async** keyword for every function you want to use this in.
+ * 
+ * @param {Number} userLocation - Takes a latitude and longitude, [required].
+ * @param {Number} userRadius   - Takes a user radius, in meters. [optional]. 
+ * @example
+ * let apiHelper = await createAPIHelper(Latitude_Longitude, Radius_In_Meters);
+
+ * @method **apiInit( )**, **getRandomRestaurant( )**
+ * @author **Rudy P.**
+ * @field **apiDataObject**
  */
 
-class APIHelper {
+async function createAPIHelper(userLocation, userRadius) {
+        /**
+         * Contains the APIHelper class. which contains various functions to assist in the manipulation of the API
+         * @param {Number} userLocation - Takes a latitude and longitude, [required].
+         * @param {Number} userRadius   - Takes a user radius, in meters. [optional].
+         * @example 
+         * let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
+         * @methods **apiInit( )**, **getRandomRestaurant( )**
+         * @field **apiDataObject**
+         */
+    class APIHelper {
+        apiDataObject; // This data object contains the API data.
+        // create a private restaurant list. 
+        #restaurauntList;
+        
+        constructor(userLocation, userRadius) {
+            this.#restaurauntList = [];
+            this.userLocation = userLocation;
+            this.userRadius = userRadius;
+        }
+        /**
+         * This method initializes the API, to call it use the await keyword, since it's an asyncronous function.
+         * You only need to call this once. This will return the apiDataObject, however, you can now use different
+         *  methods inside the APIHelper class. If you want to access the data without using the 
+         * API ****(Not recommended)****, just use the apiDataObject field. 
+         * @example await apiHelper.apiInit();
+         */
+        async apiInit() {
 
-    /**
-     * This is the main helper function I created to help us parse and work with the google places API,
-     * currently a couple of values are set as default and can't be changed, however, all you'd have to
-     * do is add them as parameters (if you wanted to set them by the user), and then add the relevant
-     * setting to the list of API parameters. As is, this is supposed to return a bunch of default data 
-     * that restraunteur needs to correctly display the relevant data.
-     * 
-     * @param {Number} userLocation - This takes a user's latitude, longitude as an
-     *                                array, example: [lat,lng]
-     * 
-     * @param {Number} userRadius   - This takes a user's radius in meters, although, if the rankBy 
-     *                                setting (WIP) is set to distance, this can't be included
-     * 
-     */
-    constructor(userLocation, userRadius) {
-        this.userLocation = userLocation;
-        this.userRadius = userRadius;
-    }
+            if (process.env.NODE_ENV !== 'production') {
+                // Checks the node environment, if it's not the production server, it won't require this configuration since we shouldn't be using a .env file in a production app, just during development.
+                require('dotenv').config();
+                // Don't remove this from here if you're deploying., this is required to make the API key work.
+            }
 
-    async nearbyPlaces() {
+            const {
+                Client,
+                defaultAxiosInstance
+            } = require("@googlemaps/google-maps-services-js"); // Initializing the client and an axios instance.
 
-        if (process.env.NODE_ENV !== 'production') {
-            // Checks the node environment, if it's not the production server, it won't require this configuration since we shouldn't be using a .env file in a production app, just during development.
-            require('dotenv').config();
-            // Don't remove this from here if you're deploying., this is required to make the API key work.
+            const client = new Client({}); // Google API Client should be private per instance that I use it.
+            this.userLocation = [45.409274, -122.722615]
+            // Set as the default for testing purposes, points to an area that has a lot of food places nearby it. TODO: Remove this later. 
+
+            // Oh, also, for future research, the pageToken parameter has some promise.
+            let clientOutput = await client.placesNearby({
+                        params: {
+                            location: this.userLocation,
+                            radius: this.userRadius,
+                            type: "food", // play around with this parameter, because it may not get back every specific restaurant that I need, especially if it's not called a restaurant.
+                            key: process.env.GOOGLE_MAPS_API_KEY
+                        },
+                        timeout: 2000 // milliseconds to wait for the request, otherwise, send an error.
+                    },
+                    defaultAxiosInstance // I still dunno what an axios instance is, but the API needs it.
+                )
+                .catch(error => {
+                    // if there's an error, catch the error and throw it.
+                    console.log(error);
+                    throw error;
+                });
+
+            this.apiDataObject = clientOutput.data.results;
+            // console.log(clientOutput); // This is for debug purposes, I'm still not returning anything yet.
+            return clientOutput.data.results; // This returns the json object back to where it's called.
+        }
+        /**
+         * Returns a random restaurant 
+         */
+        getRandomRestaurant() {
+            console.log("== Class Content");
+            console.log(this.apiDataObject);
         }
 
-        const {
-            Client,
-            defaultAxiosInstance
-        } = require("@googlemaps/google-maps-services-js"); // Initializing the client and an axios instance. 
-
-        const client = new Client({}); // Google API Client should be private per instance that I use it.
-        this.userLocation = [45.409274, -122.722615] 
-        // Set as the default for testing purposes, points to an area that has a lot of food places nearby it. TODO: Remove this later. 
-
-        // Oh, also, for future research, the pageToken parameter has some promise.
-        let clientOutput = await client.placesNearby({
-                    params: {
-                        location: this.userLocation,
-                        radius: this.userRadius,
-                        type: "restaurant", // play around with this parameter, because it may not get back every specific restaurant that I need, especially if it's not called a restaurant.
-                        key: process.env.GOOGLE_MAPS_API_KEY
-                    },
-                    timeout: 2000 // milliseconds to wait for the request, otherwise, send an error. 
-                },
-                defaultAxiosInstance // I still dunno what an axios instance is, but the API needs it.
-            )
-            .catch((error) => {
-                // if there's an error, catch the error and throw it.
-                console.log(error.response.data.error_message);
-                throw error;
-            });
-        
-        // console.log(clientOutput); // This is for debug purposes, I'm still not returning anything yet.
-        return clientOutput.data.results; // This returns the json object back to where it's called.
+        // End of class.
     }
+    let apiHelper = new APIHelper(userLocation, userRadius);
+    await apiHelper.apiInit(); // initialize it.
+    return apiHelper; // return it.
 }
 
-
-module.exports = APIHelper;
+module.exports = createAPIHelper;
--- server.js
+++ server.js
@@ -7,40 +7,65 @@
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-const APIHelper = require('./APIHelper');
-
+const createAPIHelper = require('./APIHelper');
+const latitudeLongitude = [45.409274, -122.722615];
 var app = express();
 var port = 3000;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
 
-app.get('/', function (req, res) {
-    console.log("CS290 work in project");
-    res.status(200);
+app.get('/', async function (req, res) {
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
+    apiHelper.getRandomRestaurant();
+
+    res.status(200).render('homepage', {
+        placesObj: placesObj
+    });
 });
 
 app.get('/test', async function (req, res) {
     
     apiHelper = new APIHelper([45.409274, -122.722615], 6000);
 
-    let placesObj = await apiHelper.nearbyPlaces();
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
+    apiHelper.getRandomRestaurant();
 
-    console.log("== Data Recieved");
-    console.log(placesObj);
     res.status(200);
 });
 
-app.get('*', function(req, res) {
+app.post('/rightRestaurant', async function (req, res) {
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000); // creates an instance of the API Helper class.
+
+    let placesObj = apiHelper.apiDataObject; // this gets the places object.
+
+    res.status(200).render('homepage', { //This should at some point actually make it so that restrauntprofile.handlebars is displayed
+        placesObj: placesObj //Modification probably needs to be in the homepage.handlebars file
+    });
+});
+
+app.post('/leftRestaurant', async function (req, res) {
+    let apiHelper = await createAPIHelper(latitudeLongitude, 3000);
+
+    let placesObj = apiHelper.apiDataObject; 
+
+    res.status(200).render('homepage', {
+        placesObj: placesObj
+    });
+});
+
+app.get('*', function (req, res) {
     console.log("== 404 Page Would Be displayed");
     res.status(404);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("== Server is listening on port", port);
 });
-
-//Test
