89cca23c0f850af4ca62b35691a0304cb33cb183
stuartallen <stuartallen@users.noreply.github.com>
2020-12-08 20:18:54 -0800

    Left and right buttons send server request
    -left and right buttons send server request
    -I believe the handlebars compiling code doesn't need to happen because it doesn't effect anything (I just commented it out just in case)

--- public/script.js
+++ public/script.js
@@ -1,1 +1,41 @@
-// This is the main driver code, all of us should NOT be working on this at one time.
+/*
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['leftCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+})();
+*/
+
+document.getElementById("left-button").addEventListener("click", leftButtonClick);
+document.getElementById("right-button").addEventListener("click", rightButtonClick);
+
+
+function leftButtonClick() {
+	var postRequest = new XMLHttpRequest();
+	var reqURL = "/leftRestaurant";
+	postRequest.open("POST", reqURL);
+
+	postRequest.addEventListener('load', function (event) {
+		if (event.target.status !== 200) {
+		  alert("Error storing photo in database: " + event.target.response);
+		}
+	  });
+
+	postRequest.send();
+
+}
+
+function rightButtonClick() {
+	var postRequest = new XMLHttpRequest();
+	var reqURL = "/rightRestaurant";
+	postRequest.open("POST", reqURL);
+
+	postRequest.addEventListener('load', function (event) {
+		if (event.target.status !== 200) {
+		  alert("Error storing photo in database: " + event.target.response);
+		}
+	  });
+
+	postRequest.send();
+}
