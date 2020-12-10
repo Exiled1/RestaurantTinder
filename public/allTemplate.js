039da92106b66cd2cc9d150bde4952ca6e3204ba
stuartallen <stuartallen@users.noreply.github.com>
2020-12-10 13:22:15 -0800

    Can swipe through restaurants
    - restaurants data is added to the DOM using javascript on left swipe
    - removed hidden class on things inside the modal background because it doesn't need it

--- public/allTemplate.js
+++ public/allTemplate.js
@@ -1,7 +1,7 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['buttons'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div id=\"button-container\"> \r\n	<button type=\"button\" id=\"left-button\">&lt-</button>\r\n	<button type=\"button\" id=\"right-button\">-&gt</i></button>\r\n</div>\r\n";
+    return "<div id=\"button-container\"> \n	<button type=\"button\" id=\"left-button\">&lt-</button>\n	<button type=\"button\" id=\"right-button\">-&gt</i></button>\n</div>\n";
 },"useData":true});
 templates['leftCard'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -13,7 +13,7 @@ templates['leftCard'] = template({"1":function(container,depth0,helpers,partials
 
   return "		<div>"
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? lookupProperty(depth0,"restaurant") : depth0)) != null ? lookupProperty(stack1,"name") : stack1), depth0))
-    + "</div>\r\n";
+    + "</div>\n";
 },"3":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -24,7 +24,7 @@ templates['leftCard'] = template({"1":function(container,depth0,helpers,partials
 
   return "		<div>Rating: "
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? lookupProperty(depth0,"restaurant") : depth0)) != null ? lookupProperty(stack1,"rating") : stack1), depth0))
-    + "</div>\r\n";
+    + "</div>\n";
 },"5":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -35,7 +35,7 @@ templates['leftCard'] = template({"1":function(container,depth0,helpers,partials
 
   return "		<div>Total Reviews: "
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? lookupProperty(depth0,"restaurant") : depth0)) != null ? lookupProperty(stack1,"user_ratings_total") : stack1), depth0))
-    + "</div>\r\n";
+    + "</div>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -44,13 +44,13 @@ templates['leftCard'] = template({"1":function(container,depth0,helpers,partials
         return undefined
     };
 
-  return "<div id=\"left-card-container\" class=\"card-container\">\r\n"
+  return "<div id=\"left-card-container\" class=\"card-container\">\n"
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"name") : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":4,"column":8}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"rating") : stack1),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":5,"column":1},"end":{"line":7,"column":8}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"totalReviews") : stack1),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":8,"column":1},"end":{"line":10,"column":8}}})) != null ? stack1 : "")
-    + "		<!-- TODO add photo	-->\r\n</div>\r\n";
+    + "		<!-- TODO add photo	-->\n</div>\n";
 },"useData":true});
-templates['restaurantprofile'] = template({"1":function(container,depth0,helpers,partials,data) {
+templates['restaurantProfile'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
           return parent[propertyName];
@@ -125,7 +125,7 @@ templates['restaurantprofile'] = template({"1":function(container,depth0,helpers
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"price") : stack1),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":16,"column":2},"end":{"line":18,"column":9}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"dineIn") : stack1),{"name":"if","hash":{},"fn":container.program(11, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":19,"column":2},"end":{"line":21,"column":9}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"takeOut") : stack1),{"name":"if","hash":{},"fn":container.program(13, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":22,"column":2},"end":{"line":24,"column":9}}})) != null ? stack1 : "")
-    + "		\r\n	</div>\r\n	<div id=\"modal-matched-bar\" class=\"hidden\">\r\n		It's a Match!\r\n	</div>\r\n</div>";
+    + "		\r\n	</div>\r\n	<div id=\"modal-matched-bar\" class=\"hidden\">\r\n		It's a Match!!\r\n	</div>\r\n</div>";
 },"useData":true});
 templates['rightCard'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
@@ -137,7 +137,7 @@ templates['rightCard'] = template({"1":function(container,depth0,helpers,partial
 
   return "		<div>Open: "
     + container.escapeExpression(container.lambda(((stack1 = ((stack1 = (depth0 != null ? lookupProperty(depth0,"restaurant") : depth0)) != null ? lookupProperty(stack1,"opening_hours") : stack1)) != null ? lookupProperty(stack1,"open_now") : stack1), depth0))
-    + "</div>\r\n";
+    + "</div>\n";
 },"3":function(container,depth0,helpers,partials,data) {
     var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -148,11 +148,11 @@ templates['rightCard'] = template({"1":function(container,depth0,helpers,partial
 
   return "		<div>Pricing: "
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? lookupProperty(depth0,"restaurant") : depth0)) != null ? lookupProperty(stack1,"price_level") : stack1), depth0))
-    + "</div>\r\n";
+    + "</div>\n";
 },"5":function(container,depth0,helpers,partials,data) {
-    return "		<div>Dine in</div>\r\n";
+    return "		<div>Dine in</div>\n";
 },"7":function(container,depth0,helpers,partials,data) {
-    return "		<div>Take out</div>\r\n";
+    return "		<div>Take out</div>\n";
 },"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), lookupProperty = container.lookupProperty || function(parent, propertyName) {
         if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
@@ -161,12 +161,12 @@ templates['rightCard'] = template({"1":function(container,depth0,helpers,partial
         return undefined
     };
 
-  return "<div id=\"right-card-container\" class=\"card-container\">\r\n"
+  return "<div id=\"right-card-container\" class=\"card-container\">\n"
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"open") : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":1},"end":{"line":4,"column":8}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"price") : stack1),{"name":"if","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":5,"column":1},"end":{"line":7,"column":8}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"dineIn") : stack1),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":8,"column":1},"end":{"line":10,"column":8}}})) != null ? stack1 : "")
     + ((stack1 = lookupProperty(helpers,"if").call(alias1,((stack1 = (depth0 != null ? lookupProperty(depth0,"fields") : depth0)) != null ? lookupProperty(stack1,"takeOut") : stack1),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":11,"column":1},"end":{"line":13,"column":8}}})) != null ? stack1 : "")
-    + "</div>\r\n";
+    + "</div>\n";
 },"useData":true});
 templates['settingsmenu'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
     return "";
--- public/script.js
+++ public/script.js
@@ -7,6 +7,8 @@ templates['leftCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(cont
 })();
 */
 
+//const { render } = require("sass");
+
 var latitude = 0;
 var longitude = 0;
 
@@ -33,10 +35,140 @@ function leftButtonClick() {
 	postRequest.addEventListener('load', function (event) {
 		if (event.target.status !== 200) {
 		  alert("Could not load page: " + event.target.response);
+		} else {
+			let serverResponse = JSON.parse(event.target.response);
+			console.log(serverResponse);
+			document.getElementById("left-card-container").remove();
+			document.getElementById("right-card-container").remove();
+			document.getElementById("modal-background").remove();
+
+			newLeftCard(serverResponse);
+			newRightCard(serverResponse);
+			newRestaurantProfile(serverResponse);
 		}
-	  });
+	});
 
 	postRequest.send(requestBody);
+}
+
+function newRestaurantProfile(response) {
+	var fields = response.fields;
+	var restaurant = response.restaurant;
+
+	var newProfile = document.createElement("div");
+	newProfile.classList.add("hidden");
+	newProfile.setAttribute("id", "modal-background");
+
+	var newMenu = document.createElement("div");
+	newMenu.setAttribute("id", "modal-menu");
+
+	var newbtn = document.createElement("button");
+	newbtn.setAttribute("id", "modal-close-button");
+	newbtn.textContent = "x";
+	newMenu.appendChild(newbtn);
+
+	if(fields.name) {
+		var nameDiv = document.createElement("div");
+		nameDiv.textContent = restaurant.name;
+		newMenu.appendChild(nameDiv);
+	}
+	if(fields.rating) {
+		var ratingDiv = document.createElement("div");
+		ratingDiv.textContent = "Rating: " + restaurant.rating;
+		newMenu.appendChild(ratingDiv);
+	}
+	if(fields.totalReviews) {
+		var numReviewsDiv = document.createElement("div");
+		numReviewsDiv.textContent = "Total Reviews: " + restaurant.user_ratings_total;
+		newMenu.appendChild(numReviewsDiv);
+	}
+	if(fields.open) {
+		var hoursDiv = document.createElement("div");
+		hoursDiv.textContent = restaurant.opening_hours.open_now;
+		newMenu.appendChild(hoursDiv);
+	}
+	if(fields.price) {
+		var priceDiv = document.createElement("div");
+		priceDiv.textContent = "Pricing: " + restaurant.price_level;
+		newMenu.appendChild(priceDiv);
+	}
+	if(fields.dineIn) {
+		var dineInDiv = document.createElement("div");
+		dineInDiv.textContent = "Dine in";
+		newMenu.appendChild(dinInDiv);
+	}
+	if(fields.takeOut) {
+		var takeOutDiv = document.createElement("div");
+		takeOutDiv.textContent = "Take Out";
+		newMenu.appendChild(takeOutDiv);
+	}
+
+	var matchedBar = document.createElement("div");
+	matchedBar.setAttribute("id","modal-matched-bar");
+	matchedBar.textContent = "It's a Match!!";
+
+	newProfile.appendChild(newMenu);
+
+	document.body.appendChild(newProfile);
+}
+
+function newLeftCard(response) {
+	var fields = response.fields;
+	var restaurant = response.restaurant;
+
+	var newLeftCardContainer = document.createElement("div");
+	newLeftCardContainer.classList.add("card-container");
+	newLeftCardContainer.setAttribute("id", "left-card-container");
+
+	if(fields.name) {
+		var nameDiv = document.createElement("div");
+		nameDiv.textContent = restaurant.name;
+		newLeftCardContainer.appendChild(nameDiv);
+	}
+	if(fields.rating) {
+		var ratingDiv = document.createElement("div");
+		ratingDiv.textContent = "Rating: " + restaurant.rating;
+		newLeftCardContainer.appendChild(ratingDiv);
+	}
+	if(fields.totalReviews) {
+		var numReviewsDiv = document.createElement("div");
+		numReviewsDiv.textContent = "Total Reviews: " + restaurant.user_ratings_total;
+		newLeftCardContainer.appendChild(numReviewsDiv);
+	}
+
+	document.getElementById("card-content-container").appendChild(newLeftCardContainer);
+}
+
+function newRightCard(response) {
+	var fields = response.fields;
+	var restaurant = response.restaurant;
+
+	var newRightCardContainer = document.createElement("div");
+	newRightCardContainer.classList.add("card-container");
+	newRightCardContainer.setAttribute("id", "right-card-container");
+
+	if(fields.open) {
+		var hoursDiv = document.createElement("div");
+		hoursDiv.textContent = restaurant.opening_hours.open_now;
+		newRightCardContainer.appendChild(hoursDiv);
+	}
+	if(fields.price) {
+		var priceDiv = document.createElement("div");
+		priceDiv.textContent = "Pricing: " + restaurant.price_level;
+		newRightCardContainer.appendChild(priceDiv);
+	}
+	if(fields.dineIn) {
+		var dineInDiv = document.createElement("div");
+		dineInDiv.textContent = "Dine in";
+		newRightCardContainer.appendChild(dinInDiv);
+	}
+	if(fields.takeOut) {
+		var takeOutDiv = document.createElement("div");
+		takeOutDiv.textContent = "Take Out";
+		newRightCardContainer.appendChild(takeOutDiv);
+	}
+
+	document.getElementById("card-content-container").appendChild(newRightCardContainer);
 }
 
 function positionRetrieved(position) {
@@ -50,8 +182,6 @@ function positionError(error) {
 
 function rightButtonClick() {
 	document.getElementById("modal-background").classList.remove("hidden");
-	document.getElementById("modal-menu").classList.remove("hidden");
-	document.getElementById("modal-matched-bar").classList.remove("hidden");
 
 	document.getElementById("modal-close-button").addEventListener("click", hideModal);
 	document.addEventListener('keydown',
